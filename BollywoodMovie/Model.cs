c6cf71ca8e7bc2cc9a39a15b93916c3ae76acb60
jacksonhakizimana <jackhanjackson@gmail.com>
2019-02-25 09:50:06 +0200

    first commit

---
+++ BollywoodMovie/Model.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace BollywoodMovieApp.NewDb
+{
+    public class BollywoodMovieContext : DbContext
+    {
+        public DbSet<BollywoodMovietbl> BollywoodMovies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.BollywoodMovieApp.NewDb;Trusted_Connection=True;");
+        }
+    }
+
+    public class BollywoodMovietbl
+    {
+        [Key]
+        public int BollywoodMovieId { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string category_name { get; set; }
+    }
+
+
+}
---
+++ BollywoodMovie/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BollywoodMovieApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BollywoodMovieContext())
+            {
+                Console.WriteLine("enter a title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter a category");
+                string category = Console.ReadLine();
+                Console.WriteLine("enter a year");
+                string year = Console.ReadLine();
+
+                db.BollywoodMovies.Add(new BollywoodMovietbl { title = title });
+                db.BollywoodMovies.Add(new BollywoodMovietbl { year = year  });
+                db.BollywoodMovies.Add(new BollywoodMovietbl { category_name = category });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var BollywoodMovietbl in db.BollywoodMovies)
+                {
+                    Console.WriteLine("{0}{1}{2}", BollywoodMovietbl.title, BollywoodMovietbl.year, BollywoodMovietbl.category_name);
+                }
+            }
+        }
+    }
+}
